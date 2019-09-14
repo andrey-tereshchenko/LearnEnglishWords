feb6857109bee40c86a0f793fc02f586384d7bfa
Andrey <avtogol1998@gmail.com>
2019-09-14 11:18:29 +0300

    Changed database settings

--- LearnEnglishWords/settings.py
+++ LearnEnglishWords/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'LearnEnglishWords.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'learn_english',
+        'USER': 'admin_1',
+        'PASSWORD': '1234512345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
